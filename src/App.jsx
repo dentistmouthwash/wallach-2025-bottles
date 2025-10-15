37c74a3259bc63b9e468241a8440194039557b7f
Btreecode <104658377+Btreecode@users.noreply.github.com>
2025-10-15 01:15:39 +0000

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/home";
+import ListPage from "./pages/list";
 
 function App() {
-
   return (
-    <div style={{ padding: 0, margin: 0 }}>
-      <div style={{ textAlign: "center", fontSize: 36, backgroundColor: "cyan" }}>
-        Home
-      </div>
-      <div>
-
+    <div class="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/list" element={<ListPage />} />
+        </Routes>
+      </main>
+      <div class="bottom-nav">
+        <a href="/">HOME</a>
+        <a href="/list">LIST</a>
+        <div>MENU3</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router";
+
+export default function HomePage() {
+
+  return (
+    <div style={{ padding: 0, margin: 0 }}>
+      <div style={{ textAlign: "center", fontSize: 36, backgroundColor: "cyan" }}>
+        Home
+      </div>
+      <div>
+        <Link to="/list">click here for the list</Link>
+      </div>
+    </div>
+  )
+}
