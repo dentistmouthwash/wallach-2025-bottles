396352840138651f54b5cbe798dec942aaf2c1ce
Btreecode <104658377+Btreecode@users.noreply.github.com>
2025-10-15 01:43:13 +0000

    table -> grid

--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -1,19 +1,63 @@
+import { Fragment } from "react";
+
 export default function ListPage() {
-    return <div class="bigList">
-        <table>
-            <tr>
-                <th>
-                    List
-                </th>
-            </tr>
-            <tr>
-                <td>
-                    Test
-                </td>
-                <td>
-                    Test2
-                </td>
-            </tr>
-        </table>
+  const DATA = [
+    {
+      name: "coca cola (200ml)",
+      used: true,
+      amt: 5,
+    },
+    {
+      name: "pepsi (400ml)",
+      used: true,
+      amt: 5,
+    },
+    {
+      name: "OJ (200ml)",
+      used: true,
+      amt: 5,
+    },
+    {
+      name: "sprite (500ml)",
+      used: false,
+      amt: 2,
+    },
+    {
+      name: "fanta (330ml)",
+      used: true,
+      amt: 7,
+    },
+    {
+      name: "pepsi (1L)",
+      used: false,
+      amt: 1,
+    },
+    {
+      name: "mountain dew (250ml)",
+      used: true,
+      amt: 3,
+    },
+    {
+      name: "coca cola (500ml)",
+      used: false,
+      amt: 4,
+    },
+  ];
+
+  return (
+    <div class="bigList">
+      <div class="grid-1">
+        <div style={{
+            gridColumn: "1 / span 3",
+        }}>list</div>
+        {DATA.map((item, index) => (
+          <Fragment key={index}>
+            <div>{item.name}</div>
+            <div>-</div>
+            <div>{item.amt}</div>
+          </Fragment>
+        ))}
+      </div>
     </div>
+  );
 }
