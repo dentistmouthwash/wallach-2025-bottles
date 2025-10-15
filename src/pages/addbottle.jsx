e0567aba0e7ba380e38d17d15678ba2425f678ab
dentistmouthwash <116688318+dentistmouthwash@users.noreply.github.com>
2025-10-15 01:32:01 +0000

    table

---
+++ src/pages/addbottle.jsx
@@ -0,0 +1,13 @@
+export default function bottleAdder() {
+
+    return (
+        <div style={{ padding: 0, margin: 0 }}>
+            <div style={{ textAlign: "center", fontSize: 36, backgroundColor: "cyan" }}>
+                Home
+            </div>
+            <div>
+                <Link to="/list">click here for the list</Link>
+            </div>
+        </div>
+    )
+}
--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -1,3 +1,19 @@
 export default function ListPage() {
-    return <div>some list</div>
+    return <div class="bigList">
+        <table>
+            <tr>
+                <th>
+                    List
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    Test
+                </td>
+                <td>
+                    Test2
+                </td>
+            </tr>
+        </table>
+    </div>
 }
